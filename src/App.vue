75b234b0dd21d988e77a2c35391a2591c4b0b782
Angga Yuda <anggayudap@outlook.com>
2023-11-23 05:54:23 +0700

    init,feature: install w/ vue-cli & data - method

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h2>Welcome, He Who Remains ({{ nama }})</h2>
+  <button @click="changeName">Change Name</button>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+    // HelloWorld
+  // }
+  data() {
+    return {
+      nama: "Loki"
+    }
+  },
+  methods: {
+    changeName(){
+      this.nama = "Kang";
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
